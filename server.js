ca9e96a4493d3ba17974ebad73bc3a8a9b018b02
codemonkeyspoon <98781257+codemonkeyspoon@users.noreply.github.com>
2023-04-30 06:28:59 -0400

    added start menu to cli

--- server.js
+++ server.js
@@ -13,4 +13,53 @@ const connection = mysql.createConnection({
 connection.connect(err => {
   if (err) throw err;
   console.log("WELCOME TO PAWNEE CITY HALL EMPLOYEE TRACKER");
+  startMenu();
 });
+
+const startMenu = () => {
+    inquirer.prompt({
+        message: 'What would you like to do today?',
+        name: 'menu',
+        type: 'list',
+        choices: [ 
+          'View all departments',
+          'View all jobs',
+          'View all employees',
+          'Add a department',
+          'Add a job',
+          'Add an employee',
+          'Update employee job',
+          'Exit',
+        ],
+      })
+      .then(response => {
+          switch (response.menu) {
+          case 'View all departments':
+            viewDepartment();
+            break;
+          case 'View all jobs':
+            viewJobs();
+            break;
+          case 'View all employees':
+            viewEmployees();
+            break;
+          case 'Add a department':
+            addDepartment();
+            break;
+          case 'Add a job':
+            addJob();
+            break;
+          case 'Add an employee':
+            addEmployee();
+            break;
+          case 'Update employee job':
+            updateEmployee();
+            break;
+          case "Exit":
+            connection.end();
+            break;
+          default:
+            connection.end();
+        }
+      });
+  };
