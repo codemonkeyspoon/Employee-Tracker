14fe5fc9e037f5e4156fbb5f3e5a34e3db3a2bf6
codemonkeyspoon <98781257+codemonkeyspoon@users.noreply.github.com>
2023-04-30 06:33:16 -0400

    added functions to view all departments jobs and employees

--- server.js
+++ server.js
@@ -63,3 +63,30 @@ const startMenu = () => {
         }
       });
   };
+
+  const viewDepartment = () => {
+    connection.query('SELECT * FROM department', function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      startMenu();
+    });
+  };
+  
+  const viewJobs = () => {
+    connection.query('SELECT * FROM job', function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      startMenu();
+    });
+  };
+  
+  const viewEmployees = () => {
+    connection.query(
+      'SELECT employee.id, first_name, last_name, title, salary, dept_name, manager_id FROM ((department JOIN job ON department.id = job.department_id) JOIN employee ON job.id = employee.job_id);',
+      function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        startMenu();
+      }
+    );
+  };
